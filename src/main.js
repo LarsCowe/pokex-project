5e0238940c0c68b666d46e323f88d6d6c478590e
Lars <lars.cowe@hotmail.com>
2025-05-09 18:08:03 +0200

    add pokemon details

--- src/main.js
+++ src/main.js
@@ -3,43 +3,48 @@
 // Een lijst met Pokémon ophalen
 async function fetchPokemon() {
   try {
-    // Ophalen van eerste 20 Pokémon
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
-    const data = await response.json();
+    // Ophalen van eerste 10 Pokémon (minder om te beginnen)
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
 
     if (!response.ok) {
       throw new Error(`API fout: ${response.status}`);
     }
 
-    console.log('Pokémon lijst:', data);
+    const data = await response.json();
 
-    // De resultaten tonen op de pagina
-    displayPokemon(data.results);
+    // Voor elke Pokémon de details ophalen
+    const pokemonDetailsPromises = data.results.map((pokemon) =>
+      fetch(pokemon.url).then((res) => res.json())
+    );
+
+    // Wachten tot alle details zijn opgehaald
+    const pokemonDetails = await Promise.all(pokemonDetailsPromises);
+
+    // De gedetailleerde resultaten tonen
+    displayPokemonDetails(pokemonDetails);
   } catch (error) {
     console.error('Fout bij het ophalen van Pokémon:', error);
   }
 }
 
-// Pokémon weergeven in de DOM
-function displayPokemon(pokemonList) {
+// Gedetailleerde Pokémon info weergeven
+function displayPokemonDetails(pokemonList) {
   const container = document.getElementById('pokemon-list');
+  container.innerHTML = ''; // Container leegmaken
 
   // Voor elke Pokémon
   pokemonList.forEach((pokemon) => {
-    // ID uit URL halen (laatste nummer in URL)
-    const id = pokemon.url
-      .split('/')
-      .filter((part) => part !== '')
-      .pop();
-
-    // Een kaart maken voor de Pokémon
+    // Een kaart maken
     const card = document.createElement('div');
     card.classList.add('pokemon-card');
 
-    // HTML voor de kaart
+    // HTML voor de kaart met meer details
     card.innerHTML = `
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="${pokemon.name}">
+      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
       <h3>${pokemon.name}</h3>
+      <p>Type: ${pokemon.types.map((type) => type.type.name).join(', ')}</p>
+      <p>Gewicht: ${pokemon.weight / 10} kg</p>
+      <p>Hoogte: ${pokemon.height / 10} m</p>
     `;
 
     // Toevoegen aan de container
