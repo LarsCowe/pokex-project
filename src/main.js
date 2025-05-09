e34315348c05f97a6d20534ee17c76281656d77d
Lars <lars.cowe@hotmail.com>
2025-05-09 17:10:16 +0200

    add fundamentals

--- src/main.js
+++ src/main.js
@@ -1,24 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+'use strict';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Een lijst met Pokémon ophalen
+async function fetchPokemon() {
+  try {
+    // Ophalen van eerste 20 Pokémon
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+    const data = await response.json();
 
-setupCounter(document.querySelector('#counter'))
+    if (!response.ok) {
+      throw new Error(`API fout: ${response.status}`);
+    }
+
+    console.log('Pokémon lijst:', data);
+
+    // De resultaten tonen op de pagina
+    displayPokemon(data.results);
+  } catch (error) {
+    console.error('Fout bij het ophalen van Pokémon:', error);
+  }
+}
+
+// Pokémon weergeven in de DOM
+function displayPokemon(pokemonList) {
+  const container = document.getElementById('pokemon-list');
+
+  // Voor elke Pokémon
+  pokemonList.forEach((pokemon) => {
+    // ID uit URL halen (laatste nummer in URL)
+    const id = pokemon.url
+      .split('/')
+      .filter((part) => part !== '')
+      .pop();
+
+    // Een kaart maken voor de Pokémon
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+
+    // HTML voor de kaart
+    card.innerHTML = `
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="${pokemon.name}">
+      <h3>${pokemon.name}</h3>
+    `;
+
+    // Toevoegen aan de container
+    container.appendChild(card);
+  });
+}
+
+// De app starten wanneer de pagina is geladen
+document.addEventListener('DOMContentLoaded', fetchPokemon);
